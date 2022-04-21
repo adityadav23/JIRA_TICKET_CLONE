df9cc472f7b6ab82140111f263c00c3b4e1453bf
Aditya Yadav <aditya023.ay@gmail.com>
2022-04-21 22:52:56 +0530

    Added listener for modal coloring

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 let addBtn = document.querySelector(".add-btn")
 let modalCont = document.querySelector(".modal-cont")
 let textAreaCont = document.querySelector(".textarea-cont")
+let allPriorityColors = document.querySelectorAll(".priority-color")
+
+let colors = ["lightpink", "lightblue", "lightgreen", "black"]
+let modalPriorityColor = colors[colors.length - 1]
 let mainCont = document.querySelector(".main-cont")
+
+// flag for modal-container
+// true-> modal display
+// false-> modal hide
 let addFlag = false;
 
+//Listener for modal coloring
+allPriorityColors.forEach((colorElem, index)=>{
+    colorElem.addEventListener("click", (e)=>{
+        allPriorityColors.forEach((priorityColorElem, index)=>{
+            priorityColorElem.classList.remove("border")
+        })
+        colorElem.classList.add("border")
+    })
+})
+
+//Listener for openinng modal container
 addBtn.addEventListener('click', (e)=>{
 
     addFlag = !addFlag
